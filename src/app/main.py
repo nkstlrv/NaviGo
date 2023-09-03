d1bb3b503d7827275a850af17274320ae77bb2c3
Nikita <stlrvnk@gmail.com>
2023-09-03 19:38:13 +0300

    uvicorn autoreload setup

--- src/app/main.py
+++ src/app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from routers import index
 
@@ -7,4 +8,4 @@ app.include_router(index.router)
 
 
 if __name__ == "__main__":
-    ...
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
